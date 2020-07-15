7d28c9fa1c39ffc52b4e1c4a4454d75d09dee973
Danny Nguyen <danny@keeb.io>
2020-07-15 17:19:32 -0400

    Add more info to main page

--- src/App.tsx
+++ src/App.tsx
@@ -7,14 +7,28 @@ import "./App.css";
 function App() {
   let plate: makerjs.IModel = new SwitchPlate();
   return (
-    <div className="container">
-      <Blueprint model={plate}>
-        <h3>
-          Keyboard Plate Generator by <a href="https://github.com/keebio">
-            Keebio
-          </a>
-        </h3>
-      </Blueprint>
+    <div>
+      <div className="container">
+        <Blueprint model={plate}>
+          <h3>
+            Keyboard Plate Generator by <a href="https://github.com/keebio">
+              Keebio
+            </a>
+          </h3>
+        </Blueprint>
+      </div>
+      <div className="configuration">
+        This is currently a work in progress, and here's the list of initial
+        basic functionalities that need to be added in:
+        <h4>TODO List:</h4>
+        <ul>
+          <li>SVG/DXF export</li>
+          <li>KLE data import</li>
+          <li>Various switch and stabilizer cutouts</li>
+          <li>Cutout configuration</li>
+          <li>Save configuration</li>
+        </ul>
+      </div>
     </div>
   );
 }
