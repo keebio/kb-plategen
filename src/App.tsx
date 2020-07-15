ebffda1f25ad56b4af4ed027693abda1b0f90d8c
Danny Nguyen <danny@keeb.io>
2020-07-15 18:01:26 -0400

    Add link to Trello board

--- src/App.tsx
+++ src/App.tsx
@@ -26,12 +26,19 @@ function App() {
         basic functionalities that need to be added in:
         <h4>TODO List:</h4>
         <ul>
-          <li>SVG/DXF export options (like precision)</li>
+          <li>
+            SVG/DXF export options (like precision, line color, line width)
+          </li>
           <li>KLE data import</li>
           <li>Various switch and stabilizer cutouts</li>
           <li>Cutout configuration</li>
           <li>Save configuration</li>
         </ul>
+        <p>
+          <a href="https://trello.com/b/Kfx0hbyo/plate-generator">
+            Project status board
+          </a>
+        </p>
       </div>
     </div>
   );
