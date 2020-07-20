a320eb8feaac29bd7219159506c3f1a8f5361636
Danny Nguyen <danny@keeb.io>
2020-07-20 17:41:16 -0400

    Switch to using ergodox as example

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import FileSaver from "file-saver";
 import "./App.css";
 
 function App() {
-  let sampleJson = require("./sample/atreus.json");
+  let sampleJson = require("./sample/ergodox.json");
   let switchPlate: makerjs.IModel = new SwitchPlate(sampleJson);
   return (
     <div>
