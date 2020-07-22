0fc4a3ca4e665380e7f140f6fea6bbcc47d6bd50
Danny Nguyen <danny@keeb.io>
2020-07-22 13:12:21 -0400

    Add option to mark flipped stabilizer by marking it as a homing key in KLE instead of manually editing JSON

--- src/KeyCutouts.ts
+++ src/KeyCutouts.ts
@@ -31,10 +31,18 @@ class KeyCutouts implements makerjs.IModel {
 
     let stabCutoutStyle = "normal";
     if (key.width >= 2) {
-      let stabModel = new StabilizerCutout(key.width, stabCutoutStyle, key.rs);
+      let stabModel = new StabilizerCutout(
+        key.width,
+        stabCutoutStyle,
+        key.rs || key.nub,
+      );
       models["stabilizer"] = stabModel;
     } else if (key.height >= 2) {
-      let stabModel = new StabilizerCutout(key.height, stabCutoutStyle, key.rs);
+      let stabModel = new StabilizerCutout(
+        key.height,
+        stabCutoutStyle,
+        key.rs || key.nub,
+      );
       let rotation = key.rotation_angle >= 0 ? -90 : 90;
       makerjs.model.rotate(stabModel, rotation);
       models["stabilizer"] = stabModel;
