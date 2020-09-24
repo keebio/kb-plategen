ba684983675aed95c711d021afbda890fe92ff48
Danny Nguyen <danny@keeb.io>
2020-09-24 17:18:09 -0400

    Convert PlateViewer from class component to functional component

--- src/components/PlateViewer.tsx
+++ src/components/PlateViewer.tsx
@@ -7,57 +7,53 @@ export type PlateProps = {
   switchPlate: makerjs.IModel;
 };
 
-class PlateViewer extends React.Component<PlateProps> {
-  render() {
-    return (
-      <div className="plateviewer">
-        <Blueprint
-          model={this.props.switchPlate}
-          options={{ fitOnScreen: true }}
-        >
-          <h3>
-            Keyboard Plate Generator by <a href="https://github.com/keebio">
-              Keebio
-            </a>
-          </h3>
-        </Blueprint>
-        <p />
-        <button
-          className="ui primary button"
-          onClick={(e) => this.saveSvg(this.props.switchPlate)}
-        >
-          <i className="download icon" />Download SVG
-        </button>
-        <button
-          className="ui primary button"
-          onClick={(e) => this.saveDxf(this.props.switchPlate)}
-        >
-          <i className="download icon" />Download DXF
-        </button>
-      </div>
-    );
-  }
+const PlateViewer: React.FC<PlateProps> = ({ switchPlate }) => (
+  <div className="plateviewer">
+    <Blueprint
+      model={switchPlate}
+      options={{ fitOnScreen: true }}
+    >
+      <h3>
+        Keyboard Plate Generator by <a href="https://github.com/keebio">
+          Keebio
+        </a>
+      </h3>
+    </Blueprint>
+    <p />
+    <button
+      className="ui primary button"
+      onClick={(e) => saveSvg(switchPlate)}
+    >
+      <i className="download icon" />Download SVG
+    </button>
+    <button
+      className="ui primary button"
+      onClick={(e) => saveDxf(switchPlate)}
+    >
+      <i className="download icon" />Download DXF
+    </button>
+  </div>
+);
 
-  saveSvg(model: makerjs.IModel) {
-    let options: makerjs.exporter.ISVGRenderOptions = {
-      accuracy: 0.000001,
-      units: makerjs.unitType.Millimeter,
-      strokeWidth: "0.25mm",
-    };
-    let output = makerjs.exporter.toSVG(model, options);
-    let blob = new Blob([output], { type: "text/plain;charset=utf-8" });
-    FileSaver.saveAs(blob, "kbplate.svg");
-  }
+function saveSvg(model: makerjs.IModel) {
+  let options: makerjs.exporter.ISVGRenderOptions = {
+    accuracy: 0.000001,
+    units: makerjs.unitType.Millimeter,
+    strokeWidth: "0.25mm",
+  };
+  let output = makerjs.exporter.toSVG(model, options);
+  let blob = new Blob([output], { type: "text/plain;charset=utf-8" });
+  FileSaver.saveAs(blob, "kbplate.svg");
+}
 
-  saveDxf(model: makerjs.IModel) {
-    let options: makerjs.exporter.IDXFRenderOptions = {
-      accuracy: 0.000001,
-      units: makerjs.unitType.Millimeter,
-    };
-    let output = makerjs.exporter.toDXF(model, options);
-    let blob = new Blob([output], { type: "text/plain;charset=utf-8" });
-    FileSaver.saveAs(blob, "kbplate.dxf");
-  }
+function saveDxf(model: makerjs.IModel) {
+  let options: makerjs.exporter.IDXFRenderOptions = {
+    accuracy: 0.000001,
+    units: makerjs.unitType.Millimeter,
+  };
+  let output = makerjs.exporter.toDXF(model, options);
+  let blob = new Blob([output], { type: "text/plain;charset=utf-8" });
+  FileSaver.saveAs(blob, "kbplate.dxf");
 }
 
 export default PlateViewer;
