c12c7dc7829c87691049882a195dd38c72b550cc
Danny Nguyen <danny@keeb.io>
2020-09-24 09:53:11 -0400

    Add some project notes

--- src/App.tsx
+++ src/App.tsx
@@ -52,25 +52,69 @@ class App extends React.Component<{}, AppState> {
             {...this.state}
             onConfigChange={this.handleConfigurationChange}
           />
+          &nbsp;
           <p />
+
           <div className="ui container">
-            This is currently a work in progress, and here's the list of initial
+            <h3 className="ui dividing header">
+              <i className="info circle icon" />General Information
+            </h3>
+            <h4>About</h4>
+            This is a keyboard switch plate generator that takes in output
+            from&nbsp;
+            <a href="http://www.keyboard-layout-editor.com/">
+              Keyboard Layout Editor
+            </a>&nbsp;and generates a switch plate based on the parameters you
+            specify.
+
+            <h4>Development & Contributing</h4>
+            This is currently a work in progress, and here’s the list of initial
             basic functionalities that need to be added in:
-            <h4>TODO List:</h4>
+            <p />
+            Partial Todo List:
             <ul>
               <li>
                 SVG/DXF export options (like precision, line color, line width)
               </li>
-              <li>KLE data import</li>
-              <li>Various switch and stabilizer cutouts</li>
-              <li>Cutout configuration</li>
+              <li>KLE data import from file</li>
               <li>Save configuration</li>
+              <li>Layout presets</li>
+              <li>Dark mode</li>
+              <li>And more...(See Trello board)</li>
             </ul>
-            <p>
-              <a href="https://trello.com/b/Kfx0hbyo/plate-generator">
-                Project status board
-              </a>
-            </p>
+
+            If you’d like to contribute code to this project, the GitHub
+            repository will be opened to the public shortly after some code
+            cleanup.
+            <p />
+            <a href="https://github.com/keebio/kb-plategen">
+              <button className="ui github button">
+                <i className="github icon" />GitHub
+              </button>
+            </a>
+            <p />
+
+            If you’d like to support this project monetarily, you can become a
+            sponsor! Any amount is appreciated.
+            <p />
+            <a href="https://github.com/sponsors/nooges">
+              <button className="ui purple button">
+                <i className="heart icon" />Sponsor project
+              </button>
+            </a>
+
+            <h4>Project Status & Future Features</h4>
+            <ul>
+              <li>
+                <a href="https://trello.com/b/Kfx0hbyo/plate-generator">
+                  Project status board
+                </a>
+              </li>
+              <li>GitHub Issue Tracker</li>
+            </ul>
+
+            <p />
+            &nbsp;
           </div>
         </div>
       </div>
