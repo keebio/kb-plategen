cc87fc62c57c7c662bc2ab1ca13cf934a69cdfc1
Ryan Magoon <mr.magoon5@gmail.com>
2020-11-10 17:50:52 -0500

    add eslint and prettier boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
-import React, { useState } from 'react'
-import AwesomeDebouncePromise from 'awesome-debounce-promise'
-import SwitchPlate from './maker_models/SwitchPlate'
-import PlateViewer from './components/PlateViewer'
-import { SwitchCutoutType } from './maker_models/KeyCutouts'
-import { StabilizerCutoutType } from './maker_models/StabilizerCutout'
-import PlateConfiguration, {
-  PlateConfigurationProps,
-} from './components/PlateConfiguration'
-import PlateParameters from './PlateParameters'
-import AppInfo from './components/AppInfo'
-import useConstant from 'use-constant'
+import React, { useState } from 'react';
+import AwesomeDebouncePromise from 'awesome-debounce-promise';
+import SwitchPlate from './maker_models/SwitchPlate';
+import PlateViewer from './components/PlateViewer';
+import { SwitchCutoutType } from './maker_models/KeyCutouts';
+import { StabilizerCutoutType } from './maker_models/StabilizerCutout';
+import PlateConfiguration, { PlateConfigurationProps } from './components/PlateConfiguration';
+import PlateParameters from './PlateParameters';
+import AppInfo from './components/AppInfo';
+import useConstant from 'use-constant';
 
-let kleData = JSON.stringify(require('./sample/quefrency-rev2.json'))
-kleData = kleData.substring(1, kleData.length - 1)
+let kleData = JSON.stringify(require('./sample/quefrency-rev2.json'));
+kleData = kleData.substring(1, kleData.length - 1);
 
 const defaultConfig: PlateConfigurationProps = {
   kleData,
@@ -23,44 +21,36 @@ const defaultConfig: PlateConfigurationProps = {
   horizontalKeySpacing: 19.05,
   verticalKeySpacing: 19.05,
   combineOverlaps: false,
-}
-const initialSwitchPlate = new SwitchPlate(defaultConfig)
+};
+const initialSwitchPlate = new SwitchPlate(defaultConfig);
 
 function App() {
-  const [config, setConfig] = useState(defaultConfig)
-  const [switchPlate, setSwitchPlate] = useState(initialSwitchPlate)
+  const [config, setConfig] = useState(defaultConfig);
+  const [switchPlate, setSwitchPlate] = useState(initialSwitchPlate);
 
   const debouncedMakeSwitchPlate = useConstant(() =>
-    AwesomeDebouncePromise(
-      (params: PlateParameters) => new SwitchPlate(params),
-      500
-    )
-  )
+    AwesomeDebouncePromise((params: PlateParameters) => new SwitchPlate(params), 500)
+  );
 
-  const handleConfigurationChange = async (
-    newConfig: PlateConfigurationProps
-  ) => {
-    setConfig(newConfig)
-    setSwitchPlate(switchPlate)
-    const newSwitchPlate = await debouncedMakeSwitchPlate(config)
-    setConfig(newConfig)
-    setSwitchPlate(newSwitchPlate)
-  }
+  const handleConfigurationChange = async (newConfig: PlateConfigurationProps) => {
+    setConfig(newConfig);
+    setSwitchPlate(switchPlate);
+    const newSwitchPlate = await debouncedMakeSwitchPlate(config);
+    setConfig(newConfig);
+    setSwitchPlate(newSwitchPlate);
+  };
 
   return (
     <>
       <PlateViewer switchPlate={switchPlate} />
       <div>
-        <PlateConfiguration
-          {...config}
-          onConfigChange={handleConfigurationChange}
-        />
+        <PlateConfiguration {...config} onConfigChange={handleConfigurationChange} />
         &nbsp;
         <p />
         <AppInfo />
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
