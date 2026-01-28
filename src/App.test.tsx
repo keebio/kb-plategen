a44c74892413e723366fe064914e12b5f5f179db
Danny Nguyen <danny@keeb.io>
2026-01-28 12:28:12 -0500

    More updates

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom'
 import App from './App'
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />)
-  const linkElement = getByText(/keyboard switch/i)
-  expect(linkElement).toBeInTheDocument()
+test('renders app title', () => {
+  render(<App />)
+  const titleElement = screen.getByText(/keyboard plate generator/i)
+  expect(titleElement).toBeInTheDocument()
 })
